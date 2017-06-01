00d4eed14feda3a29b8c8729336145be50b48b35
Chris Klanac <cklanac@gmail.com>
2017-06-01 11:47:48 -0400

    solves initial tests

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const { DATABASE, PORT } = require('./config');
 
 const app = express();
@@ -8,6 +9,29 @@ const app = express();
 // Add middleware and .get, .post, .put and .delete endpoints
 app.get('/', (req, res) => {
   res.send('Hello World!');  
+});
+
+// Set CORS headers to enable cross-domain requests
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+  next();
+});
+app.use(bodyParser.json());
+
+/** CORE ENDPOINTS **/
+app.get('/api/items', (req, res) => {
+  res.json([]);
+});
+
+app.post('/api/items', (req, res) => {
+  // Solution A: Use .location()
+  res.status(201).location('PLACEHOLDER').json({ title: req.body.title });
+
+  // Solution B: use .setHeader()
+  // res.setHeader('location', 'PLACEHOLDER');
+  // res.status(201).json({ title: req.body.title });
 });
 
 let server;
